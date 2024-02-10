bb189114112173691f3dcefe774f01a92c1ee12b
Em-kale <emmittluhning@outlook.com>
2024-02-10 00:33:46 -0500

    Add a prompt and prompt template called on local ollama mixtral server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Repository for experimenting with langchain using typescript
+
+To run: 
+- Must have Ollama installed, with mistral
+- Run 
--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
-const world = "world";
+import { ChatOllama } from "@langchain/community/chat_models/ollama";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
-export function hello(who: string = world): void {
-  console.log(`Hello, ${who}!`);
+const chatModel = new ChatOllama({
+  baseUrl: "http://localhost:11434", // Default value
+  model: "mistral",
+});
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a newspaper reported from the early twentieth century"],
+  ["user", "{input}"],
+]);
+const chain = prompt.pipe(chatModel);
+
+async function doPrompt() {
+  let result = await chain.invoke({ input: "what is langchain?" });
+  console.log(result.content);
+  //TODO: convert this output to a string instead of a message
 }
 
-hello();
+doPrompt();
