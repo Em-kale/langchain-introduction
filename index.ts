4e288c8123ed4d99d1af75f5a3306cefa010a645
Em-kale <emmittluhning@outlook.com>
2024-02-13 09:41:04 -0500

    Added ability for user to specify custom prompt

--- index.ts
+++ index.ts
@@ -4,36 +4,53 @@ import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createRetrievalChain } from "langchain/chains/retrieval";
 import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
 import { FaissStore } from "@langchain/community/vectorstores/faiss";
+import * as readline from "node:readline";
 
-console.log("starting the program");
+function initializeAndBegin(): void {
+  console.log("starting the program");
 
-console.log("initializing the chat model...");
-//this is the chat model that we will use to generate the responses
-const chatModel = new ChatOllama({
-  baseUrl: "http://localhost:11434", // Default value
-  model: "mistral",
-});
+  console.log("initializing the chat model...");
+  //this is the chat model that we will use to generate the responses
+  const chatModel = new ChatOllama({
+    baseUrl: "http://localhost:11434", // Default value
+    model: "mistral",
+  });
 
-//essentially sets what messages have happened alrady
-const prompt = ChatPromptTemplate.fromMessages([
-  [
-    "system",
-    "Answer the user's questions based on the below context:\n\n{context}",
-  ],
-  ["user", "{input}"],
-]);
+  const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  read.question(`Enter Inquiry: `, async (input) => {
+    //essentially sets what messages have happened alrady
+    const promptTemplate = ChatPromptTemplate.fromMessages([
+      [
+        "system",
+        "Answer the user's questions based on the below context:\n\n{context}",
+      ],
+      ["user", "{input}"],
+    ]);
+
+    await doPrompt(promptTemplate, input, chatModel);
+    read.close();
+  });
+}
 
 async function loadVectorStore(): Promise<FaissStore> {
   console.log("loading the vector store...");
   const vectorStore = await FaissStore.load(
-    "./vector_store/",
+    "vector_store",
     new OllamaEmbeddings({ model: "mistral" })
   );
   console.log("finished loading the vector store.");
   return vectorStore;
 }
 
-async function doPrompt(){
+async function doPrompt(
+  prompt: any,
+  userInput: string,
+  chatModel: ChatOllama
+): Promise<void> {
   const loadedVectorStore = await loadVectorStore();
 
   console.log("`creating document chain...");
@@ -56,11 +73,9 @@ async function doPrompt(){
   });
 
   console.log("invoking retrieval chain...");
-  const result = await retrievalChain.invoke({
-    input: "what is vital point ai",
-  });
+  const result = await retrievalChain.invoke({ input: userInput });
   console.log("finished.");
   console.log(result);
 }
 
-doPrompt();
+initializeAndBegin();
